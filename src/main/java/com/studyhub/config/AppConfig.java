e9a58841af21cee78a215a3033b583d5347f2503
Satyam Mishra <satyyam.misshra1121@gmail.com>
2024-08-17 08:21:49 +0530

    AppConfig added

---
+++ src/main/java/com/studyhub/config/AppConfig.java
@@ -0,0 +1,24 @@
+package com.studyhub.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public Storage storage() throws IOException {
+        InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream("serviceAccountKey.json");
+        if (serviceAccount == null) {
+            throw new IOException("Service account key file not found");
+        }
+        GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
+        return StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+    }
+}
