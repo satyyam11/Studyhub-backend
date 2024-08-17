14a3a3f1f2a2b4d2776eb416e3a6144bfd046b75
Satyam Mishra <satyyam.misshra1121@gmail.com>
2024-08-17 08:17:29 +0530

    Add AuthFilter

---
+++ src/main/java/com/studyhub/config/AuthFilter.java
@@ -0,0 +1,45 @@
+package com.studyhub.config;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseToken;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+public class AuthFilter extends OncePerRequestFilter {
+
+    private final FirebaseAuth firebaseAuth;
+
+    public AuthFilter(FirebaseAuth firebaseAuth) {
+        this.firebaseAuth = firebaseAuth;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            String idToken = authorizationHeader.substring(7);
+            try {
+                FirebaseToken decodedToken = firebaseAuth.verifyIdToken(idToken);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        decodedToken.getUid(), null, Collections.emptyList());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch (Exception e) {
+                SecurityContextHolder.clearContext();
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid token");
+                return;
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
