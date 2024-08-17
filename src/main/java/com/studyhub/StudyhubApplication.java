bfa144e6a06c94743c8ee7376c9bfb02a5f40c75
Satyam Mishra <satyyam.misshra1121@gmail.com>
2024-08-17 08:30:33 +0530

    updated

--- src/main/java/com/studyhub/StudyhubApplication.java
+++ src/main/java/com/studyhub/StudyhubApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudyhubApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudyhubApplication.class, args);
 	}
