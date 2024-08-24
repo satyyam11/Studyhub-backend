165565cb66f3412cbf36ea61c0f95940099d65d4
Satyam Mishra <satyyam.misshra1121@gmail.com>
2024-08-24 20:00:21 +0530

    Resolved merge conflicts

---
+++ src/main/java/com/studyhub/model/ChatMessage.java
@@ -0,0 +1,33 @@
+package com.studyhub.model;
+
+public class ChatMessage {
+    private String content;
+    private String sender;
+    private String type;
+
+    // getters and setters
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
